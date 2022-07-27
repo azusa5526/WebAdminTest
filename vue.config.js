b6ba28823fbc357dcf46dfebb4c63ffa070d541a
AndrewYao <azusa5526@gmail.com>
2022-07-27 13:24:54 +0800

    chore: 加入字體、svg sprite 依賴
    調整原因：
    1.加入中文字體
    2.為了使用 svgIcon 組件
    
    調整項目：
    1.package.json 引入 svg-sprite-loader, noto-sans-tc
    2.vue.config.js 增加 svg-icon 處理
    
    關聯issue：
    #10907

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,43 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+function resolve(dir) {
+	return path.join(__dirname, dir);
+}
+
 module.exports = defineConfig({
-	transpileDependencies: ['vuetify']
+	publicPath: '/redmine',
+	transpileDependencies: ['vuetify'],
+	// css: {
+	// 	loaderOptions: {
+	// 		scss: {
+	// 			additionalData: `@import "@/scss/helpers/_theme-colors.scss";`
+	// 		}
+	// 	}
+	// },
+	productionSourceMap: false,
+	chainWebpack: (config) => {
+		config.resolve.alias.set('@', resolve('src'));
+		config.resolve.alias.set('~', resolve('node_modules'));
+
+		// del default svg setting
+		config.module.rules.delete('svg');
+
+		// add svg-sprite-loader setting
+		config.module
+			.rule('svg-sprite-loader')
+			.test(/\.svg$/)
+			.include.add(resolve('src/assets/icon'))
+			.end()
+			.use('svg-sprite-loader')
+			.loader('svg-sprite-loader')
+			.options({ symbolId: '[name]' });
+
+		// modify image-loader setting
+		config.module.rule('images').exclude.add(resolve('src/assets/icon'));
+
+		config.plugin('html').tap((args) => {
+			args[0].title = 'Redmine';
+			return args;
+		});
+	}
 });
