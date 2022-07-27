61069a19ba9fd6e77ebbd1c05516b722af16d16e
AndrewYao <azusa5526@gmail.com>
2022-07-27 13:53:37 +0800

    style: 引入與調整 vuetify 主題色

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,13 +11,16 @@ export default new Vuetify({
 		},
 		themes: {
 			light: {
-				primary: '#007BFF',
-				secondary: '#424242',
-				accent: '#82B1FF',
-				error: '#FF5252',
-				info: '#2196F3',
-				success: '#4CAF50',
-				warning: '#FFC107'
+				primary: '#074E92',
+				secondary: '#1B5E20',
+				accent: '#2196F3',
+				accentDarken1: '#1976D2',
+				accentLighten1: '#BBDEFB',
+				light: '#E8EAF6',
+				error: '#FF3D00',
+				success: '#7CB342',
+				warning: '#FF8F00',
+				disabled: '#bdbdbd'
 			}
 		}
 	},
