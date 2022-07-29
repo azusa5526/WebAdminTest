4eb1323dfd3c95376c71a7a71165e06b874d23f8
AndrewYao <azusa5526@gmail.com>
2022-07-29 14:36:03 +0800

    chore: 引入表單驗證依賴
    調整原因：
    1.加入表單驗證
    
    關聯issue：
    #10907

--- src/main.js
+++ src/main.js
@@ -4,12 +4,32 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'noto-sans-tc/all.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import SvgIcon from '@/components/widget/SvgIcon';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
+// vee-validate
+import { ValidationObserver, ValidationProvider, extend, localize, configure } from 'vee-validate';
+import TW from 'vee-validate/dist/locale/zh_TW.json';
+import * as rules from 'vee-validate/dist/rules';
+
 Vue.config.productionTip = false;
+
+// vee-validate
+Object.keys(rules).forEach((rule) => {
+	extend(rule, rules[rule]);
+});
+localize('zh_TW', TW);
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+configure({
+	classes: {
+		valid: 'is-valid',
+		invalid: 'is-invalid'
+	}
+});
 
 // require all svg
 const requireAll = (requireContext) => requireContext.keys().map(requireContext);
